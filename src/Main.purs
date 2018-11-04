21cdcc3772923bf944e70001f78be4de28519c15
John Watson <john.watson@gmx.co.uk>
2018-11-04 10:19:59 +0000

    Main now embeds the PS in a selected element
    rather than taking over the entire document.  Not sure which HTML tag to
    use (this needs to be embedded eventually in tradtunestore).  For the
    moment I've chosen a p tag.

--- src/Main.purs
+++ src/Main.purs
@@ -2,12 +2,14 @@ module Main where
 
 import Prelude
 import Effect (Effect)
+import Data.Foldable (traverse_)
 import Halogen.Aff as HA
 import Halogen.VDom.Driver (runUI)
+import Web.DOM.ParentNode (QuerySelector(..))
 
 import Container as Container
 
 main :: Effect Unit
 main = HA.runHalogenAff do
-  body <- HA.awaitBody
-  runUI Container.component unit body
+  HA.awaitLoad
+  traverse_ (runUI Container.component unit) =<< HA.selectElement (QuerySelector "p")
